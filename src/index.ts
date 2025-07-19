7a2cc091edb34b3fc145e12dbbe8ec452a1f4ff1
Thyago Euclides <flooddk@gmail.com>
2025-07-19 19:05:04 -0400

    feat: implement product management methods in ProductList class

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,58 @@
-export interface IProduct {
-  id: number;
-  name: string;
-  price: number;
-  createdAt: Date;
-  updatedAt: Date;
+import { IProduct, IProductService } from "./interfaces";
+
+class ProductList implements IProductService {
+  private productList: IProduct[] = [];
+  id: number = 1;
+
+  createProduct(data: { name: string; price: number }): IProduct {
+    const { name, price } = data;
+
+    const newProduct: IProduct = {
+      id: this.id,
+      name,
+      price,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    this.productList.push(newProduct);
+    this.id++;
+
+    return newProduct;
+  }
+
+  getProducts(): IProduct[] {
+    return this.productList;
+  }
+
+  getOneProduct(id: number): IProduct | undefined {
+    return this.productList.find((product) => product.id === id);
+  }
+
+  updateProduct(id: number, data: { name?: string; price?: number }): IProduct {
+    const productIndex = this.productList.findIndex(
+      (product) => product.id === id
+    );
+
+    const updateProduct: IProduct = {
+      ...this.productList[productIndex],
+      ...data,
+      updatedAt: new Date(),
+    };
+
+    this.productList.splice(productIndex, 1, updateProduct);
+
+    return updateProduct;
+  }
+
+  deleteProduct(id: number): { message: string } {
+    const productIndex = this.productList.findIndex(
+      (product) => product.id === id
+    );
+
+    this.productList.splice(productIndex, 1);
+    return { message: "Product sucessfully deleted." };
+  }
 }
 
-export interface IProductService {
-  createProduct(data: { name: string; price: number }): IProduct;
-  getProducts(): IProduct[];
-  getOneProduct(id: number): IProduct | undefined;
-  updateProduct(id: number, data: { name?: string; price?: number }): IProduct;
-  deleteProduct(id: number): { message: string };
-}
+export const productList = new ProductList();
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -5,3 +5,11 @@ export interface IProduct {
   createdAt: Date;
   updatedAt: Date;
 }
+
+export interface IProductService {
+  createProduct(data: { name: string; price: number }): IProduct;
+  getProducts(): IProduct[];
+  getOneProduct(id: number): IProduct | undefined;
+  updateProduct(id: number, data: { name?: string; price?: number }): IProduct;
+  deleteProduct(id: number): { message: string };
+}
